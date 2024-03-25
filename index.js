980d171423d55af1b98211dfd265f4003b6e609a
Eddiezon-lemayan <Eddymacharia450@gmail.com>
2024-03-25 14:58:16 +0300

    coplete task

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// function scuberGreetingForFeet(dis){
+//   let result
+//   if (dis<=400){
+//     result = 'This one is on me!';
+//   }
+//   else if (400 >= 2000){
+//     result = 'That will be $20 dollars.';
+//   }
+//   else if(2000 >= 2500){
+//     result = 'That will be $30 dollars'
+//   }
+//   else{
+//     resut = "Does not allow reides over 2500 feet.";
+//   }
+
+
+// }
+
+function scuberGreetingForFeet(distance){
+  let result;
+  if(distance <= 400){
+    result='This one is on me!';
+  }
+  else if(distance > 400 && distance <= 2000){
+    result='That will be twenty bucks.';
+  }
+  else if(distance > 2000 && distance <= 2500){
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if(distance > 2500){
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+let result;
+if(City == 'NYC'){
+  result = 'Ok, sounds good.'
+}
+else if (City != 'NYC')
+{
+  result = 'No go.'
+}
+return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous){
+  let result;
+  if(generous == "generous"){
+    result = 'Thank you so much.'
+  }
+  else if (generous == 'not as generous'){
+    result = 'Thank you.'
+  }
+  else{
+    result = 'Bye.'
+  }
+  
+  return result;
 }
